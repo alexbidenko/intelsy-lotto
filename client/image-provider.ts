b01c2797d38a025b2446dfe5e0724a9c74196716
Alexander Bidenko <alexbidenko1998@gmail.com>
2024-12-25 16:49:43 +0300

    Fix

--- client/image-provider.ts
+++ client/image-provider.ts
@@ -3,8 +3,7 @@ import type { ProviderGetImage } from '@nuxt/image';
 import { getImage as ipxProvider } from '#image/providers/ipx';
 
 export const getImage: ProviderGetImage = (src, options, ctx) => {
-  if (/^https?:\/\//.test(src))
-    src = src.replace(useRuntimeConfig().public.origin, '');
+  if (/^https?:\/\//.test(src)) src = `/${src.split('/').slice(3).join('/')}`;
 
   return ipxProvider(src, options, ctx);
 };
